764d5aec99a9c14a044690f7aa9b408a1e776450
Fabio Soldati <soldati@gmx.net>
2024-05-17 13:42:26 +0200

    added transport blog module

--- website/wordpress/themes/Divi-RE-WIN/functions.php
+++ website/wordpress/themes/Divi-RE-WIN/functions.php
@@ -14,4 +14,18 @@ if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
 endif;
 add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
+
+/*================================================ 
+# Load custom Blog Module. added by fso. see https://intercom.help/elegantthemes/en/articles/4532734-moving-blog-module-in-child-theme
+================================================*/ 
+
+
+function divi_transport_blog_module() { 
+     get_template_part( '/includes/TransportBlog' ); 
+     $dcfm = new ET_Builder_Module_Transport_Blog(); 
+     add_shortcode( 'et_pb_transport_blog', array( $dcfm, '_shortcode_callback' ) ); 
+} 
+    
+add_action( 'et_builder_ready', 'divi_transport_blog_module' ); 
+
 // END ENQUEUE PARENT ACTION
